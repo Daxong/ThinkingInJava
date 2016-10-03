02c15c492f6f9b87fe0663b459a79131789c71ce
ThinkingInJava <yuyong@renaissance-era.com>
2016-10-03 10:25:59 +0800

    ..

---
+++ src/JavaDoc.java
@@ -0,0 +1,18 @@
+//:object/JavaDoc.java
+import java.util.Date;
+/**The frist Thinking in Java example program
+ * Displaya a string and today's date
+ * @author ilove
+ * @version 1.0*/
+public class JavaDoc {
+	/**Entry point to class & application.
+	 * @param args array of string arguments
+	 * @throws exceptions No exceptions thrown */
+	public static void main(String[] args){
+		System.out.println("Hello,it's: ");
+		System.out.println(new Date());
+	}/* Output:(55% match)
+	Hello it's:
+	Sun Oct 02 09:53:18 CST 2016
+	*///:~
+}
